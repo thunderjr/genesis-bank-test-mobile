17936a3dc4100336389bf87aa49a37d6ffd99f54
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 20:13:12 -0300

    feat: get paginated products method

---
+++ server/features/get-paginated-products.ts
@@ -0,0 +1,42 @@
+import type { FilterQuery } from "mongoose";
+
+import type { PaginationOptions, PaginatedResult } from "../types/pagination";
+import type { IProduct } from "../types/product";
+import { Product } from "./../db/mongo/models";
+
+export type FilterProductsReturnType = PaginatedResult<IProduct> & {
+  categories: string[];
+};
+
+export const getPaginatedProducts = async (
+  { category, name }: Partial<Pick<IProduct, "category" | "name">>,
+  options: PaginationOptions
+): Promise<FilterProductsReturnType> => {
+  try {
+    const { page, limit } = options;
+    const skip = (page - 1) * limit;
+    const query: FilterQuery<IProduct> = {};
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (name) {
+      // "i" for case insensitive
+      query.name = { $regex: name, $options: "i" };
+    }
+
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query).skip(skip).limit(limit);
+    const categories = await Product.distinct("category");
+
+    return {
+      categories,
+      items: products,
+      total: totalProducts,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to filter products");
+  }
+};

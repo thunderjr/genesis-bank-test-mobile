8c5ae16e364a2b99038416813eccd132d8b0a5c8
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 23:37:44 -0300

    feat: update products screen on navigation

--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -1,5 +1,6 @@
-import { FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Text, View } from "react-native";
 import { useEffect, useState } from "react";
+import { useNavigation } from "expo-router";
 
 import { ProductCard } from "../../components/product-card";
 import { apiRequester } from "../../helpers/api-requester";
@@ -7,20 +8,32 @@ import { IProduct } from "../../validation/productSchema";
 import { Container } from "../../components/container";
 
 export default function ProductsPage() {
+  const navigation = useNavigation();
   const [products, setProducts] = useState<IProduct[]>([]);
   const [page, setPage] = useState<number>(1);
 
   const fetchData = async (pageNum: number) => {
     try {
       const res = await apiRequester(`/product?page=${pageNum}`);
-      setProducts((prevProducts) => [...prevProducts, ...res.data.items]);
+      setProducts(res.data.items);
     } catch (error) {
-      console.error("Failed to fetch products", error);
+      Alert.alert("Ops!", "Não foi possível buscar os produtos!", [
+        {
+          text: "Ok",
+          onPress: () => {},
+        },
+      ]);
     }
   };
 
   useEffect(() => {
     fetchData(page);
+
+    const unsubscribe = navigation.addListener("focus", () => {
+      fetchData(page);
+    });
+
+    return unsubscribe;
   }, []);
 
   return (

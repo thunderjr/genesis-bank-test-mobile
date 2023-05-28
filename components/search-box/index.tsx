5fcab2754dc3338a757380471b42bca632c3180a
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-28 12:01:58 -0300

    feat: product name search box

---
+++ components/search-box/index.tsx
@@ -0,0 +1,27 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+
+import { Container, SButton } from "./styles";
+import { SInput } from "../input";
+
+type Props = {
+  handleSearch: (name: string) => void;
+};
+
+export const ProductSearchBox = ({ handleSearch }: Props) => {
+  const [name, setName] = useState<string>("");
+
+  return (
+    <Container>
+      <SInput
+        placeholder="Pesquisar pelo nome..."
+        placeholderTextColor={"#AAAAAAAA"}
+        onChangeText={(value) => setName(value)}
+        full={true}
+      />
+      <SButton onPress={() => handleSearch(name)}>
+        <FontAwesome name="search" size={24} color="white" />
+      </SButton>
+    </Container>
+  );
+};
---
+++ components/search-box/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex-direction: row;
+  padding-horizontal: 10px;
+  margin-vertical: 20px;
+  gap: 5px;
+`;
+
+export const SButton = styled.TouchableOpacity`
+  background-color: rgb(65, 46, 191);
+  border-radius: 10px;
+  padding-horizontal: 10px;
+  padding-vertical: 8px;
+`;

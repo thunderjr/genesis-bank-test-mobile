a89903ad762e2fe594332b4f2de720a2750e7697
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-28 14:52:09 -0300

    feat: new cart modal with footer and items placeholder

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,21 +1,39 @@
 import { FlatList, Platform, Text, View } from "react-native";
+import styled from "styled-components/native";
 import { StatusBar } from "expo-status-bar";
 
-import { useCart } from "../context/cart-context";
+import { CartTotalFooter } from "../components/cart-total-footer";
 import { CartItemComponent } from "../components/cart-item";
+import { useCart } from "../context/cart-context";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function CartScreen() {
   const { cartItems, addToCart } = useCart();
 
   return (
-    <View>
-      <FlatList
-        data={cartItems}
-        keyExtractor={(item, index) => `cart-${index}`}
-        renderItem={({ item }) => (
-          <CartItemComponent addToCart={addToCart} item={item} />
-        )}
-      />
+    <View style={{ height: "100%" }}>
+      {cartItems.length ? (
+        <FlatList
+          data={cartItems}
+          keyExtractor={(item, index) => `cart-${index}`}
+          renderItem={({ item }) => (
+            <CartItemComponent addToCart={addToCart} item={item} />
+          )}
+        />
+      ) : (
+        <View
+          style={{
+            height: "80%",
+            alignItems: "center",
+            justifyContent: "center",
+            opacity: 0.15,
+          }}
+        >
+          <AntDesign name="shoppingcart" size={100} />
+        </View>
+      )}
+
+      <CartTotalFooter />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />

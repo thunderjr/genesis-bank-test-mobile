5cd0b68da533e10900d50e6b81c706ac3ff1f9d3
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-28 12:03:31 -0300

    feat: cart context (imported from Web)

---
+++ context/cart-context.tsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useState } from "react";
+import { IProduct } from "../validation/productSchema";
+
+export type CartItem = {
+  product: IProduct;
+  quantity: number;
+};
+
+type CartContextType = {
+  cartItems: CartItem[];
+  addToCart: (product: IProduct, quantity: number) => void;
+  removeFromCart: (product: IProduct) => void;
+  clearCart: () => void;
+};
+
+const CartContext = createContext<CartContextType>({
+  cartItems: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+  clearCart: () => {},
+});
+
+export const CartProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (product: IProduct, quantity: number) => {
+    const existingItem = cartItems.find((item) => item.product === product);
+
+    if (existingItem) {
+      const newQuantity = existingItem.quantity + quantity;
+
+      if (newQuantity > 0) {
+        setCartItems((prevItems) =>
+          prevItems.map((item) =>
+            item.product === product ? { ...item, quantity: newQuantity } : item
+          )
+        );
+      } else {
+        removeFromCart(product);
+      }
+    } else if (quantity > 0) {
+      setCartItems((prevItems) => [...prevItems, { product, quantity }]);
+    }
+  };
+
+  const removeFromCart = (product: IProduct) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.product !== product)
+    );
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);

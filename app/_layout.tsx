2ba1c18f0d710f5bb6853af75b69d5a926aac6cf
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 23:40:02 -0300

    chore: remove react-navigation package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import { SplashScreen, Stack } from "expo-router";
 export { ErrorBoundary } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import { ThemeProvider, DefaultTheme } from "@react-navigation/native";
 
 export const unstable_settings = {
   initialRouteName: "(tabs)",
@@ -29,12 +28,10 @@ export default function RootLayout() {
 function RootLayoutNav() {
   return (
     <>
-      <ThemeProvider value={DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="cart" options={{ presentation: "modal" }} />
-        </Stack>
-      </ThemeProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+      </Stack>
     </>
   );
 }

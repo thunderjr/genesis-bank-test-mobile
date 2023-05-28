32c8f95b047d9e7410ba3bf26bc4ee2caed1361c
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 22:51:14 -0300

    chore: remove rest of color scheme implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,9 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { SplashScreen, Stack } from "expo-router";
-import { useColorScheme } from "react-native";
 export { ErrorBoundary } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import {
-  ThemeProvider,
-  DefaultTheme,
-  DarkTheme,
-} from "@react-navigation/native";
+import { ThemeProvider, DefaultTheme } from "@react-navigation/native";
 
 export const unstable_settings = {
   initialRouteName: "(tabs)",
@@ -32,11 +27,9 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
     <>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="cart" options={{ presentation: "modal" }} />
